506663c5432ed4bccd015671c13dd2f108f34a06
mashang123456 <865909475@qq.com>
2017-06-11 21:15:01 +0800

    Initial commit

---
+++ app/src/main/java/com/ue/news_listview/bean/NewsBean.java
@@ -0,0 +1,15 @@
+package com.ue.news_listview.bean;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * bean类用来设置数据的属性，可理解为一个结构体
+ */
+
+public class NewsBean {
+    public String title;  //新闻标题
+    public String des;    //新闻描述
+    public Drawable icon; //图标
+    public String news_url;//新闻链接
+}
+
